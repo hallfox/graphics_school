2a47df41307b39b7b505ae44bc03763c06553d97
Taylor <lxmachina@gmail.com>
2016-03-24 20:26:51 -0400

    Camera still wonky but Part B done

--- Foxhall_CS460_HW3/main.cpp
+++ Foxhall_CS460_HW3/main.cpp
@@ -4,20 +4,48 @@
 
 #include <iostream>
 #include <assert.h>
+#include <math.h>
 
 namespace {
-  float camera_z = 40.0;
-  float theta_x = 0.0;
-  float theta_y = 0.0;
-  float theta_z = 0.0;
+  double camera_x = 0.0;
+  double camera_y = 0.0;
+  double camera_z = 40.0;
+  double theta_z = 0.0;
+  double fairy_x = 0.0;
+  double fairy_y = 0.0;
+  double fairy_z = camera_z - 1;
+  double lever_rot = 0.0;
   GLsizei width;
   GLsizei height;
+  enum Scene { SCENE_CYLINDER, SCENE_LEVER };
+  Scene current_scene = SCENE_CYLINDER;
+}
+
+void reset_camera() {
+  camera_x = 0.0;
+  camera_y = 0.0;
+  camera_z = 40.0;
+  fairy_x = 0.0;
+  fairy_y = 0.0;
+  fairy_z = camera_z - 1;
+  theta_z = 0.0;
+  lever_rot = 0.0;
+}
+
+void next_scene() {
+  reset_camera();
+  if (current_scene == SCENE_CYLINDER) {
+    current_scene = SCENE_LEVER;
+  } else {
+    current_scene = SCENE_CYLINDER;
+  }
 }
 
 void init() {
   glClearColor(0.0, 0.0, 0.0, 1.0);
   //glLineWidth(2.0);
   glShadeModel(GL_FLAT);
+  reset_camera();
 }
 
 void draw_ground() {
@@ -35,11 +63,14 @@ void draw_ground() {
   glEnd();
 }
 
-void draw_scene() {
+void draw_cylinder(float rx, float ry, float rz) {
+  // Draw ground
+  draw_ground();
+
   glPushMatrix();
-  glRotatef(theta_x, 1, 0, 0);
-  glRotatef(theta_y, 0, 1, 0);
-  glRotatef(theta_z, 0, 0, 1);
+  glRotatef(rx, 1, 0, 0);
+  glRotatef(ry, 0, 1, 0);
+  glRotatef(rz, 0, 0, 1);
 
   // Draw cylinder
   GLUquadricObj *quad = gluNewQuadric();
@@ -49,9 +80,79 @@ void draw_scene() {
   gluCylinder(quad, 5, 5, 30, 20, 10);
   gluDeleteQuadric(quad);
 
+  glPopMatrix();
+}
+
+void draw_lever(float rx, float ry, float rz) {
   // Draw ground
   draw_ground();
+
+  glPushMatrix();
+  glRotatef(rx, 1, 0, 0);
+  glRotatef(ry, 0, 1, 0);
+  glRotatef(rz, 0, 0, 1);
+
+  GLUquadricObj *quad = gluNewQuadric();
+  gluQuadricDrawStyle(quad, GLU_FILL);
+
+  glPushMatrix();
+  glRotated(lever_rot, 0, 1, 0); // rotation for b2
+  // Position s1
+  glRotated(90, 1, 0, 0);
+  // Draw s1
+  glColor3f(0.0, 0.0, 1.0);
+  gluCylinder(quad, 1, 1, 20, 10, 10);
+  // Position b2
+  // Draw b2
+  glColor3f(0.0, 1.0, 1.0);
+  gluSphere(quad, 2, 10, 10);
   glPopMatrix();
+
+
+  // left arm
+  glPushMatrix();
+  glRotated(-90+lever_rot, 0, 1, 0);
+  glColor3f(0.0, 0.0, 1.0);
+  gluCylinder(quad, 1, 1, 10, 10, 10); // s2
+  glTranslated(0.0, 0.0, 10.0);
+  glRotated(90, 0, 1, 0);
+  glRotated(lever_rot+90, 1, 0, 0);
+  glColor3f(0.0, 1.0, 1.0);
+  gluSphere(quad, 1.5, 10, 10); // b1
+  glColor3f(0.0, 0.0, 1.0);
+  gluCylinder(quad, 0, 1, 10, 10, 10); // s2
+  glTranslated(0, 0, 10);
+  glColor3f(0.0, 1.0, 1.0);
+  gluSphere(quad, 3, 10, 10); // b1
+  glPopMatrix();
+
+  // right arm
+  glPushMatrix();
+  glRotated(90+lever_rot, 0, 1, 0);
+  glColor3f(0.0, 0.0, 1.0);
+  gluCylinder(quad, 1, 1, 10, 10, 10); // s2
+  glTranslated(0.0, 0.0, 10.0);
+  glRotated(90, 0, 1, 0);
+  glRotated(lever_rot+90, 1, 0, 0);
+  glColor3f(0.0, 1.0, 1.0);
+  gluSphere(quad, 1.5, 10, 10); // b1
+  glColor3f(0.0, 0.0, 1.0);
+  gluCylinder(quad, 0, 1, 10, 10, 10); // s2
+  glTranslated(0, 0, 10);
+  glColor3f(0.0, 1.0, 1.0);
+  gluSphere(quad, 3, 10, 10); // b1
+  glPopMatrix();
+
+  gluDeleteQuadric(quad);
+  glPopMatrix();
+}
+
+void draw_scene(float rx, float ry, float rz) {
+  if (current_scene == SCENE_CYLINDER) {
+    draw_cylinder(rx, ry, rz);
+  } else if (current_scene == SCENE_LEVER) {
+    draw_lever(rx, ry, rz);
+  }
 }
 
 void draw_perpective(float x, float y, float z, float u, float v, float n) {
@@ -67,7 +168,7 @@ void draw_perpective(float x, float y, float z, float u, float v, float n) {
   glLoadIdentity();
   // Camera adjustment
   gluLookAt(x, y, z, 0.0, 0.0, 0.0, u, v, n);
-  draw_scene();
+  draw_scene(0.0, 0.0, 0.0);
 }
 
 void draw_side_views() {
@@ -101,8 +202,18 @@ void display() {
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
   // Camera adjustment
-  gluLookAt(0.0, 0.0, camera_z, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-  draw_scene();
+  gluLookAt(camera_x, camera_y, camera_z, fairy_x, fairy_y, fairy_z, 0.0, 1.0, 0.0);
+  draw_scene(0, 0, theta_z);
+  // Draw fairy
+  glPushMatrix();
+  GLUquadricObj *quad = gluNewQuadric();
+  gluQuadricDrawStyle(quad, GLU_LINE);
+  glTranslated(fairy_x, fairy_y, fairy_z);
+  glColor3f(1.0, 0.0, 1.0);
+  glLineWidth(1.0);
+  gluSphere(quad, 0.1, 10, 10);
+  gluDeleteQuadric(quad);
+  glPopMatrix();
 
   draw_side_views();
 
@@ -132,15 +243,34 @@ void reshape(int w, int h) {
 }
 
 void keyboard_handler(unsigned char key, int, int) {
+  double delta_x;
+  double delta_y;
+  double delta_z;
   switch (key) {
     case 27: // ESC
       exit(0);
       break;
-    case 'z':
-      camera_z += 10;
+    case 'w':
+      delta_x = (fairy_x - camera_x) * 10;
+      delta_y = (fairy_y - camera_y) * 10;
+      delta_z = (fairy_z - camera_z) * 10;
+      fairy_x += delta_x;
+      fairy_y += delta_y;
+      fairy_z += delta_z;
+      camera_x += delta_x;
+      camera_y += delta_y;
+      camera_z += delta_z;
       break;
-    case 'Z':
-      camera_z -= 10;
+    case 's':
+      delta_x = (fairy_x - camera_x) * -10;
+      delta_y = (fairy_y - camera_y) * -10;
+      delta_z = (fairy_z - camera_z) * -10;
+      fairy_x += delta_x;
+      fairy_y += delta_y;
+      fairy_z += delta_z;
+      camera_x += delta_x;
+      camera_y += delta_y;
+      camera_z += delta_z;
       break;
     case 'r':
       theta_z += 10;
@@ -148,28 +278,72 @@ void keyboard_handler(unsigned char key, int, int) {
     case 'R':
       theta_z -= 10;
       break;
+    case 'n':
+      next_scene();
+      break;
+    case 'g':
+      lever_rot += 10;
+      break;
+    case 'G':
+      lever_rot -= 10;
     default:
       break;
   }
 }
 
+void rotate(double& x, double& y, double theta) {
+  double t = cos(theta)*x - sin(theta)*y;
+  y = sin(theta)*x + cos(theta)*y;
+  x = t;
+}
+
 void special_key_handler(int key, int, int) {
+  double rot_angle = 0.174533;
+  //double sin_theta = sin(rot_angle);
+  //double cos_theta = cos(rot_angle);
+  double look_angle;
   switch (key) {
     case GLUT_KEY_UP:
-      theta_x += 10;
-      if (theta_x > 360) theta_x = -360;
+      fairy_y -= camera_y;
+      fairy_z -= camera_z;
+      look_angle = atan(fairy_x/fairy_z);
+      rotate(fairy_x, fairy_z, look_angle);
+      rotate(fairy_y, fairy_z, rot_angle);
+      rotate(fairy_x, fairy_z, -look_angle);
+      fairy_y += camera_y;
+      fairy_z += camera_z;
+      //fairy_y = cos_theta*fairy_y - sin_theta*fairy_z + camera_y;
+      //fairy_z = sin_theta*fairy_y + cos_theta*fairy_z + camera_z;
       break;
     case GLUT_KEY_DOWN:
-      theta_x -= 10;
-      if (theta_x < -360) theta_x = 360;
+      fairy_y -= camera_y;
+      fairy_z -= camera_z;
+      look_angle = atan(fairy_x/fairy_z);
+      rotate(fairy_x, fairy_z, look_angle);
+      rotate(fairy_y, fairy_z, -rot_angle);
+      rotate(fairy_x, fairy_z, -look_angle);
+      fairy_y += camera_y;
+      fairy_z += camera_z;
+      //fairy_y = cos_theta*fairy_y + sin_theta*fairy_z + camera_y;
+      //fairy_z = -sin_theta*fairy_y + cos_theta*fairy_z + camera_z;
       break;
     case GLUT_KEY_RIGHT:
-      theta_y -= 10;
-      if (theta_y < -360) theta_y = 360;
+      fairy_x -= camera_x;
+      fairy_z -= camera_z;
+      rotate(fairy_x, fairy_z, rot_angle);
+      fairy_y += camera_y;
+      fairy_z += camera_z;
+      //fairy_x = cos_theta*fairy_x - sin_theta*fairy_z + camera_x;
+      //fairy_z = sin_theta*fairy_x + cos_theta*fairy_z + camera_z;
       break;
     case GLUT_KEY_LEFT:
-      theta_y += 10;
-      if (theta_y > 360) theta_y = -360;
+      fairy_x -= camera_x;
+      fairy_z -= camera_z;
+      rotate(fairy_x, fairy_z, -rot_angle);
+      fairy_y += camera_y;
+      fairy_z += camera_z;
+      //fairy_x = cos_theta*fairy_x + sin_theta*fairy_z + camera_x;
+      //fairy_z = -sin_theta*fairy_x + cos_theta*fairy_z + camera_z;
       break;
     default:
       break;
